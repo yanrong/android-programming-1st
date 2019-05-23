4cfbc0cf1aed856e0c0c7a6b0e99c6d36d520375
dyanrong <dyanrong@unimaxcomm.com>
2019-05-23 11:45:05 +0800

    update CriminalIntent-14-sqlite for keep time select
    Change-Id: I5f9fdf42b32b02cc7946da5f2b82f347efc1e43f

--- CriminalIntent-14-sqlite/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDbSchema.java
+++ CriminalIntent-14-sqlite/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDbSchema.java
@@ -8,6 +8,7 @@ public class CrimeDbSchema {
             public static final String UUID = "uuid";
             public static final String TITLE = "title";
             public static final String DATE = "date";
+            public static final String TIME = "time";
             public static final String SOLVED = "solved";
         }
     }
