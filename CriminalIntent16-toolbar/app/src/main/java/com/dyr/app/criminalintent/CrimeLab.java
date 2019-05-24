be6ac988dbd36e9236e459a601fb6faead9351a8
yanrong <yanrongdu@gmail.com>
2019-05-24 23:47:59 +0800

    add chapter 16 code The Big Nerd Ranch Guid

---
+++ CriminalIntent16-toolbar/app/src/main/java/com/dyr/app/criminalintent/CrimeLab.java
@@ -0,0 +1,41 @@
+package com.dyr.app.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+    private ArrayList<Crime> mCrimes;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<Crime>();
+    }
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime c : mCrimes) {
+            if (c.getId().equals(id))
+                return c;
+        }
+        return null;
+    }
+
+}
