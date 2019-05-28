8d65786261390e5434a3d03afb473ff6a928bd1c
yanrong <yanrongdu@gmail.com>
2019-05-28 23:30:01 +0800

    update chapter 18 code The Big Nerd Ranch Guid

--- CriminalIntent18-action/app/src/main/java/com/dyr/app/criminalintent/CrimeListFragment.java
+++ CriminalIntent18-action/app/src/main/java/com/dyr/app/criminalintent/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -52,13 +54,57 @@ public class CrimeListFragment extends ListFragment {
         }
 
         ListView listView = (ListView) v.findViewById(android.R.id.list);
-        registerForContextMenu(listView);
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
             // float context menu
             registerForContextMenu(listView);
         }else{
             //contextual action bar
            listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+           listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+               @Override
+               public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+                   //ignore it
+               }
+
+               @Override
+               public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                   MenuInflater  inflater = actionMode.getMenuInflater();
+                   inflater.inflate(R.menu.crime_list_item_context, menu);
+                   return true;
+               }
+
+               @Override
+               public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                   return false;
+               }
+
+               @Override
+               public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                   switch (menuItem.getItemId()){
+                       case R.id.menu_item_delete_crime:
+                           CrimeAdapter adapter = (CrimeAdapter)getListAdapter();
+                           CrimeLab crimeLab = CrimeLab.get(getActivity());
+
+                           for(int i = adapter.getCount() - 1; i >= 0; i--){
+                               if(getListView().isItemChecked(i)){
+                                   crimeLab.deleteCrime(adapter.getItem(i));
+                               }
+                           }
+
+                           actionMode.finish();
+                           adapter.notifyDataSetChanged();
+                           return  true;
+
+                       default:
+                           return false;
+                   }
+               }
+
+               @Override
+               public void onDestroyActionMode(ActionMode actionMode) {
+
+               }
+           });
         }
         return v;
     }
