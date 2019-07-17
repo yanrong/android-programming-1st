e82aef1f60fe48ff9c7a667bd5826acecab28a95
yanrong <yanrongdu@gmail.com>
2019-07-17 22:50:14 +0800

    add chapater 29 src  code

---
+++ PhotoGallery29/app/src/main/java/com/dyr/photogallery/FlickerFetchr.java
@@ -0,0 +1,121 @@
+package com.dyr.photogallery;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class FlickerFetchr {
+    public static final String TAG = "FlickrFetchr";
+
+    public static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String API_KEY="key";
+    private static final String METHOD_GET_RECENT = "";
+    private static final String METHOD_SEARCH = "search";
+    private static final String PARAM_EXTRAS = "extras";
+    private static final String PARAM_TEXT = "text";
+    private static final String ENDPOINT = "https://image.baidu.com/";
+    private static final String EXTRA_SMALL_URL = "url_s";
+    private static final String XML_PHOTO = "photo";
+
+    byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        //HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+
+        try{
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+                return null;
+            }
+
+            int byteRead = 0;
+            byte[] buffer = new byte[1024];
+            while((byteRead = in.read(buffer)) > 0){
+                out.write(buffer,0, byteRead);
+            }
+            out.close();
+            return out.toByteArray();
+        }finally {
+          connection.disconnect();
+        }
+    }
+
+    public String getUrl(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public ArrayList<GalleryItem> downloadGalleryItems(String url){
+        ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
+        try{
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received xml: " + xmlString);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(xmlString));
+            parserItems(items, parser);
+        }catch (IOException e){
+            Log.e(TAG, "Failed to fetch items", e);
+        }catch (XmlPullParserException xpe){
+            Log.e(TAG, "xml parser failed", xpe);
+        }
+
+        return items;
+    }
+
+    public ArrayList<GalleryItem> fetchItems(){
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                .appendQueryParameter("method", METHOD_GET_RECENT)
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                .build().toString();
+
+        return downloadGalleryItems(url);
+    }
+
+    public ArrayList<GalleryItem> search(String query){
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                .appendQueryParameter("method", METHOD_SEARCH)
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter(PARAM_TEXT, EXTRA_SMALL_URL)
+                .build().toString();
+
+        return downloadGalleryItems(url);
+    }
+
+    void parserItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws
+            XmlPullParserException, IOException{
+        int eventType = parser.getEventType();
+
+        while(eventType != XmlPullParser.END_DOCUMENT){
+            if(eventType == XmlPullParser.START_TAG && XML_PHOTO.equals(parser.getName())){
+                String id = parser.getAttributeValue(null, "id");
+                String caption = parser.getAttributeValue(null,"title");
+                String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+
+                GalleryItem item = new GalleryItem();
+                item.setId(id);
+                item.setCaption(caption);
+                item.setUrl(smallUrl);
+                items.add(item);
+            }
+
+            eventType = parser.next();
+        }
+    }
+
+}
