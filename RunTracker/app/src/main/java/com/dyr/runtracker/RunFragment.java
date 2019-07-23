39e1d55b59dc93966723ab9e5edcfe9fbd287c24
yanrong <yanrongdu@gmail.com>
2019-07-23 23:14:58 +0800

    update chapter 33 code ,but it dose't work

--- RunTracker/app/src/main/java/com/dyr/runtracker/RunFragment.java
+++ RunTracker/app/src/main/java/com/dyr/runtracker/RunFragment.java
@@ -1,18 +1,26 @@
 package com.dyr.runtracker;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RunFragment extends Fragment {
     private Button mStartButton, mStopButton;
     private TextView mStartTextView, mLatitudeTextView, mLongitudeTextView,
-            mAltitudeTextView, mDurationtTextView;
+            mAltitudeTextView, mDurationTextView;
     private RunManager mRunManger;
+    private Location mLastLocation;
+    private Run mRun;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -29,7 +37,7 @@ public class RunFragment extends Fragment {
         mLatitudeTextView   = (TextView) v.findViewById(R.id.run_latitudeTextView);
         mLongitudeTextView  = (TextView) v.findViewById(R.id.run_longitudeTextView);
         mAltitudeTextView   = (TextView) v.findViewById(R.id.run_altitudeTextView);
-        mDurationtTextView  = (TextView) v.findViewById(R.id.run_durationTextView);
+        mDurationTextView  = (TextView) v.findViewById(R.id.run_durationTextView);
 
         mStartButton    = (Button) v.findViewById(R.id.run_startButton);
         mStopButton     = (Button) v.findViewById(R.id.run_stopButton);
@@ -38,6 +46,7 @@ public class RunFragment extends Fragment {
             @Override
             public void onClick(View v){
                 mRunManger.startLocationUpdates();
+                mRun = new Run();
                 updateUI();
             }
         });
@@ -51,8 +60,49 @@ public class RunFragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        getActivity().registerReceiver(mLocationReceiver, new IntentFilter(RunManager.ACTION_LOCATION));
+    }
+
+    @Override
+    public void onStop(){
+        getActivity().unregisterReceiver(mLocationReceiver);
+        super.onStop();
+    }
+
+    private BroadcastReceiver mLocationReceiver = new LocationReceiver(){
+
+        @Override
+        protected  void onLocationReceived(Context context, Location loc){
+            mLastLocation = loc;
+            if(isVisible()){
+                updateUI();
+            }
+        }
+
+        protected void onProviderEnableChanged(boolean enable){
+            int toastText = enable ? R.string.gps_enabled : R.string.gps_disabled;
+            Toast.makeText(getActivity(), toastText, Toast.LENGTH_LONG);
+        }
+    };
+
     private void updateUI(){
         boolean started = mRunManger.isTrackingRun();
+        if(mRun != null){
+            mStartTextView.setText(mRun.getStartDate().toString());
+        }
+
+        int durationSeconds = 0;
+        if(mRun != null && mLastLocation != null){
+            durationSeconds = mRun.getDurationSeconds(mLastLocation.getTime());
+            mLatitudeTextView.setText(Double.toString(mLastLocation.getLatitude()));
+            mLongitudeTextView.setText(Double.toString(mLastLocation.getLongitude()));
+            mAltitudeTextView.setText(Double.toString(mLastLocation.getAltitude()));
+        }
+
+        mDurationTextView.setText(Run.formatDuration(durationSeconds));
         mStartButton.setEnabled(!started);
         mStopButton.setEnabled(started);
     }
--- RunTracker/app/src/main/java/com/dyr/runtracker/RunManager.java
+++ RunTracker/app/src/main/java/com/dyr/runtracker/RunManager.java
@@ -1,15 +1,19 @@
 package com.dyr.runtracker;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 
 public class RunManager {
     private static final String TAG = "RunManager";
+    private static final String TEST_PROVIDER = "TEST_PROVIDER";
 
     public static final String ACTION_LOCATION = "com.dyr.runtracker.ACTION_LOCATION";
 
@@ -29,6 +33,11 @@ public class RunManager {
             sRunManager = new RunManager(c.getApplicationContext());
         }
 
+        ActivityCompat.requestPermissions((Activity) c, new String[] {
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION },
+                123);
+
         return sRunManager;
     }
 
@@ -38,12 +47,31 @@ public class RunManager {
         return PendingIntent.getBroadcast(mAppContext, 0, broadcast, flags);
     }
 
+    @SuppressLint("MissingPermission")
     public void startLocationUpdates() {
         String provider = LocationManager.GPS_PROVIDER;
+        //If you have the test provider and it's enabled, ues it
+        if(mLocationManager.getProvider(TEST_PROVIDER) != null &&
+        mLocationManager.isProviderEnabled(TEST_PROVIDER)){
+            provider = TEST_PROVIDER;
+        }
+        Log.d(TAG, "Using provider "+ provider);
+        //Get the last known location and broadcast it if you have one
+        Location lastKnown = mLocationManager.getLastKnownLocation(provider);
+        if(lastKnown != null){
+            lastKnown.setTime(System.currentTimeMillis());
+            broadcastLocation(lastKnown);
+        }
 
         //Start update from location manager
         PendingIntent pi = getLocationPendingIntent(true);
         mLocationManager.requestLocationUpdates(provider, 0, 0, pi);
+    }
+
+    private void broadcastLocation(Location location){
+        Intent broadcast = new Intent(ACTION_LOCATION);
+        broadcast.putExtra(LocationManager.KEY_LOCATION_CHANGED, location);
+        mAppContext.sendBroadcast(broadcast);
     }
 
     public void stopLocationUpdates(){
